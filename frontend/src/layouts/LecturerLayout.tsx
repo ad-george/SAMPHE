a9ffbbabd07b9f0ae2e157fe1bc424b14e11104d
Adomi <adomigeorge@gmail.com>
2026-09-23 23:13:56 +0300

    add global search updates on profile changes3

--- frontend/src/layouts/LecturerLayout.tsx
+++ frontend/src/layouts/LecturerLayout.tsx
@@ -264,7 +264,6 @@ const LecturerLayout = () => {
               );
             })}
           </nav>
-          <div className="flex-1" />
 
           <div
             className="p-2 md:p-3 border-t border-gray-700 relative"
@@ -283,6 +282,7 @@ const LecturerLayout = () => {
                 ) : (
                   profileForm.fullName?.charAt(0) || "L"
                 )}
+                <div className="flex-1" />
               </div>
               {!collapsed && (
                 <div className="flex-1 min-w-0 overflow-hidden">
